14f000a383b70553b16d3110805aea22f8e59058
Andrew Kennedy <akennedy2292@gmail.com>
2020-03-30 02:15:30 -0700

    add initial discord bot

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 const express = require('express')
-const app = express()
+const Discord = require('discord.js')
 
 const PORT = process.env.PORT || 3000
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 
-app.get("*", function(req, res) {
+// discord bot config
+const bot = new Discord.Client()
+bot.login(DISCORD_TOKEN)
+
+const userContent = {}
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+})
+bot.on('message', msg => {
+  console.log(msg)
+  const { author: { username } } = msg
+  if (username === 'Overseer') {
+    return
+  }
+  userContent[username] = msg.content
+})
+
+// express server config
+const app = express()
+app.get("/:username", function(req, res) {
+  const username = req.params.username
+  const content = userContent[username]
+  if (content) {
+    return res.send(content)
+  }
   res.send("tunnel snakes rule")
 })
 
